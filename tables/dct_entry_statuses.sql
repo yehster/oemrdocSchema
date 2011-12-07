73bd871a586c03f7c5819b5cf538b0e54bc2b664
yehster <kevinyeh@alum.mit.edu>
2011-12-07 16:48:28 -0500

    initial info

---
+++ tables/dct_entry_statuses.sql
@@ -0,0 +1,9 @@
+CREATE  TABLE `openemr`.`dct_entry_statuses` (
+  `uuid` VARCHAR(36) NOT NULL ,
+  `modified` DATETIME NULL ,
+  `created` DATETIME NULL ,
+  `author` VARCHAR(45) NOT NULL ,
+  `entry_id` VARCHAR(36) NOT NULL ,
+  `status` INT NOT NULL ,
+  PRIMARY KEY (`uuid`) ,
+  INDEX `entry` (`entry_id` ASC) );
