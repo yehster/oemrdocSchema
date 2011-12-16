e2b80f5e8aadf675ca18cb9d1080813226a33085
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-12-16 16:01:31 -0500

    med names usage

---
+++ tables/dct_med_names_usage.sql
@@ -0,0 +1,9 @@
+delimiter $$
+
+CREATE TABLE `dct_med_names_usage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `rxaui_id` varchar(8) NOT NULL,
+  `frequency` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8$$
+
